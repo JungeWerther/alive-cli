778a545d0a9ac2376c22d32e51347c7597fa0b5f
Seb Wiechers <sebastiaanwiechers@icloud.com>
2025-03-06 18:21:56 +0100

    feature: dynamic pgp encryption on deploy command

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -10,24 +10,48 @@
 #   - $DEPLOY_DIR
 #   - $BRANCH
 
-# Exit on any error
-set -e
+# Decrypt credentials
+GPG_TTY=$(tty)
+export GPG_TTY
+CREDENTIALS=$(gpg --decrypt $CREDENTIALS_ROOT)
 
-# Variables
-# Push to github
-echo "🔄 Pushing latest changes to GitHub..."
-git push
 
-# SSH into server and run commands
-# Optional: Replace with your own deployment logic
-echo "🚀 Deploying to server..."
-ssh $SERVER "cd $DEPLOY_DIR && \
-    git pull origin $BRANCH"
+# Export AWS credentials
+eval "$CREDENTIALS"
 
-    # DEBUG:
-    # && \
-    # mix deps.get && \
-    # mix compile && \
-    # mix phx.server restart"
+# Run command with remaining args
+shift
+$@
 
-echo "✅ Deployment complete!"
+# Clear environment variables
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_DEFAULT_REGION
+
+
+
+
+
+
+
+# # Exit on any error
+# set -e
+
+# # Variables
+# # Push to github
+# echo "🔄 Pushing latest changes to GitHub..."
+# git push
+
+# # SSH into server and run commands
+# # Optional: Replace with your own deployment logic
+# echo "🚀 Deploying to server..."
+# ssh $SERVER "cd $DEPLOY_DIR && \
+#     git pull origin $BRANCH"
+
+#     # DEBUG:
+#     # && \
+#     # mix deps.get && \
+#     # mix compile && \
+#     # mix phx.server restart"
+
+# echo "✅ Deployment complete!"
